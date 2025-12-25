76a0e6010b8accd3bdbd2e9b194adc51ebee41f5
nikitakislov <kisloff.nickita@yandex.ru>
2025-12-26 01:46:38 +0300

    refactor: clean-up .gitingore and extra files

--- scratch.ts
+++
@@ -1,8 +0,0 @@
-// scratch.ts
-import { Address } from "@ton/core";
-try {
-    const a = Address.parse('0QAC4r-gk8nlu2H9Ml0-p4Ub-vKaNFv6OUeWrMd00frQOKnY');
-    console.log("parsed:", a.toString());
-} catch (e) {
-    console.error("parse error:", e);
-}
