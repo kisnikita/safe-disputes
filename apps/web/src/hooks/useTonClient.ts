9f763130bc386da54e332bc157e20656e3079d4b
nikitakislov <kisloff.nickita@yandex.ru>
2026-01-30 20:49:51 +0300

    fix(web): change version of testnet

--- apps/web/src/hooks/useTonClient.ts
+++ apps/web/src/hooks/useTonClient.ts
@@ -1,21 +1,24 @@
-import { getHttpEndpoint } from "@orbs-network/ton-access";
+import { getHttpEndpoint, getHttpV4Endpoint } from "@orbs-network/ton-access";
 import { CHAIN } from "@tonconnect/ui-react";
-import { TonClient } from "@ton/ton";
+import { TonClient, TonClient4 } from "@ton/ton";
 import { useAsyncInitialize } from "./useAsyncInitialize";
 import { useTonConnect } from "./useTonConnect";
 
 export function useTonClient() {
-    const {network} = useTonConnect()
+    const { network } = useTonConnect()
 
     return {
-        client: useAsyncInitialize(async ()=>{
-            if(!network) return;
+        client: useAsyncInitialize(async () => {
+            if (!network) return;
 
-            return new TonClient({
-                endpoint: await getHttpEndpoint({
-                    network: network === CHAIN.MAINNET ? "mainnet" : "testnet"
+            if (network === CHAIN.MAINNET) {
+                return new TonClient({
+                    endpoint: await getHttpEndpoint({ network: "mainnet" })
                 })
-            })
+            } else
+                return new TonClient4({
+                    endpoint: await getHttpV4Endpoint({ network: "testnet" })
+                })
         }, [network])
     }
 }
