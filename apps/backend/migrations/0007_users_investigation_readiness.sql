dac8d756f243af941c3632d072469d4ba0872f14
nikitakislov <kisloff.nickita@yandex.ru>
2026-05-06 16:12:35 +0300

    fix(backend): default true investigation_readiness

--- apps/backend/migrations/0007_users_investigation_readiness.sql
+++ apps/backend/migrations/0007_users_investigation_readiness.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE users
-    ADD COLUMN IF NOT EXISTS investigation_readiness BOOLEAN NOT NULL DEFAULT TRUE;
+    ADD COLUMN IF NOT EXISTS investigation_readiness BOOLEAN NOT NULL DEFAULT FALSE;
 -- +goose StatementEnd
 
 -- +goose Down
